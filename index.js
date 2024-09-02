7d9568b9234d0f1bbf816736727537d163f60619
Natasha Shilova <natasha.shilova.89@gmail.com>
2024-09-02 15:03:34 +0300

    add fixes

--- index.js
+++ index.js
@@ -1,123 +1,118 @@
-webix.ready(function () {
-  var head = {
-    view: "toolbar",
-    css: "header",
-    id: "myToolbar",
-    height: 50,
-    padding: 10,
-    cols: [
-      { view: "label", label: "My App", css: "logo" },
-      {
-        view: "button",
-        label: "Profile",
-        type: "icon",
-        icon: "wxi-user",
-        width: 150,
-        css: "profile-icon",
-      },
-    ],
-  };
 
-  var sideBarData = [
-    { id: 1, value: "Dashboard", $css: "sidebar-item" },
-    { id: 2, value: "Users", $css: "sidebar-item" },
-    { id: 3, value: "Products", $css: "sidebar-item" },
-    { id: 4, value: "Locations", $css: "sidebar-item" },
-  ];
-
-  var sideBar = {
-    view: "list",
-    css: "side-bar",
-    width: 250,
-    scroll: false,
-    data: sideBarData,
-  };
-
-  var connectStatus = {
-    css: "connect-status",
-    height: 50,
-    template: "<span class='webix_icon wxi-check'></span> Connected",
-  };
-
-  var filmsTable = {
-    view: "datatable",
-    autoConfig: true,
-    data: small_film_set,
-  };
-
-  var formButtons = [
+const head = {
+  view: "toolbar",
+  css: "webix_dark",
+  id: "myToolbar",
+  height: 50,
+  padding: 10,
+  cols: [
+    { view: "label", label: "My App", css: "logo" },
     {
       view: "button",
-      type: "form",
-      value: "Add new",
-      css: "form-button",
+      label: "Profile",
+      type: "icon",
+      icon: "wxi-user",
+      width: 150,
+      css: "profile-icon",
     },
+  ],
+};
+
+const sideBarData = [
+  { id: 1, value: "Dashboard", $css: "sidebar-item" },
+  { id: 2, value: "Users", $css: "sidebar-item" },
+  { id: 3, value: "Products", $css: "sidebar-item" },
+  { id: 4, value: "Locations", $css: "sidebar-item" },
+];
+
+const sideBar = {
+  view: "list",
+  css: "side-bar",
+  width: 250,
+  scroll: false,
+  data: sideBarData,
+};
+
+const connectStatus = {
+  css: "connect-status",
+  height: 40,
+  template: "<span class='webix_icon wxi-check'></span> Connected",
+};
+
+const filmsTable = {
+  view: "datatable",
+  autoConfig: true,
+  data: small_film_set,
+};
+
+const formButtons = [
+  {
+    view: "button",
+    value: "Add new",
+    css: "webix_primary",
+  },
+  {
+    view: "button",
+    value: "Clear",
+  },
+];
+
+const formHeader = {
+  template: "EDIT FILMS",
+  type: "section",
+};
+
+
+const filmsForm = {
+  view: "form",
+  css: "films_form",
+  width: 300,
+  elements: [
+    formHeader,
+    { view: "text", label: "Title", name: "title" },
     {
-      view: "button",
-      type: "form",
-      value: "Clear",
+      view: "text",
+      label: "Year",
+      name: "year",
     },
-  ];
 
-  var filmsForm = {
-    view: "form",
-    borderless: true,
-    css: "films_form",
-    width: 300,
-    elements: [
-      { view: "text", label: "Title", name: "title" },
-      {
-        view: "text",
-        label: "Year",
-        name: "year",
-      },
+    { view: "text", label: "Rating", name: "rating" },
+    { view: "text", label: "Votes", name: "votes" },
+    {
+      cols: formButtons,
+    },
 
-      { view: "text", label: "Rating", name: "rating" },
-      { view: "text", label: "Votes", name: "votes" },
-      {
-        cols: formButtons,
-      },
+    {},
+  ],
+};
 
-      {},
-    ],
-  };
 
-  var formHeader = {
-    css: "form_name_wrapper",
-    height: 60,
-    template:
-      "<div class='bottom'> <div class='form-name'>EDIT FILMS<div/><div/>",
-    type: "header",
-  };
 
-  var formBody = {
-    rows: [formHeader, filmsForm],
-  };
 
-  var main = {
-    cols: [
-      {
-        css: "side-bar-container",
-        rows: [sideBar, connectStatus],
-      },
-      { view: "resizer" },
-      filmsTable,
-      formBody,
-    ],
-  };
+const formBody = {
+  rows: [filmsForm],
+};
 
-  var foot = {
-    rows: [
-      {
-        css: "footer",
-        height: 30,
-        template: `The software id provided by  <a style='margin-left:5px' href="https://webix.com" target="_blank">https://webix.com</a>. All rights reserved (c)`,
-      },
-    ],
-  };
+const main = {
+  cols: [
+    {
+      css: "side-bar-container",
+      rows: [sideBar, connectStatus],
+    },
+    { view: "resizer" },
+    filmsTable,
+    formBody,
+  ],
+};
 
-  webix.ui({
-    id: "app",
-    rows: [head, main, foot],
-  });
-});
+const foot = {
+  rows: [
+    {
+      css: "footer",
+      height: 30,
+      template: `The software id provided by  <a style='margin-left:5px' href="https://webix.com" target="_blank">https://webix.com</a>. All rights reserved (c)`,
+    },
+  ],
+};
+
+
