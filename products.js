0374b20ce1b3c6033319bc8a1f6625c99b9c293a
Natasha-eng <natasha.shilova.89@gmail.com>
2024-09-08 16:23:50 +0300

    lesson4 fixes

--- products.js
+++ products.js
@@ -1,26 +1,35 @@
 import widgetsIds from "./data_variables.js";
 
 // products
-
 const products = {
-    view: "treetable",
-    id: widgetsIds.products,
-    columns: [
-        { id: "id", header: "", css: { "text-align": "right" } },
-        {
-            id: "title", header: "Title",
-            template: "{common.treetable()} #title#",
-            fillspace: true
-        },
-        { id: "price", header: "Price", fillspace: true },
-
-    ],
-    select: "cell",
-    scrollX: false,
-    url: "./data/products.js",
-    ready: function () {
-        this.openAll();
+  view: "treetable",
+  id: widgetsIds.products,
+  editable: true,
+  columns: [
+    { id: widgetsIds.products_id, header: "", css: { "text-align": "right" } },
+    {
+      id: widgetsIds.products_title,
+      header: "Title",
+      template: "{common.treetable()} #title#",
+      fillspace: true,
+      editor: "text",
     },
-}
+    {
+      id: widgetsIds.products_price,
+      header: "Price",
+      fillspace: true,
+      editor: "text",
+    },
+  ],
+  rules: {
+    title: webix.rules.isNotEmpty,
+    price: webix.rules.isNumber,
+  },
+  scrollX: false,
+  url: "./data/products.js",
+  ready: function () {
+    this.openAll();
+  },
+};
 
 export default products;
--- users.js
+++ users.js
@@ -2,141 +2,131 @@ import widgetsIds from "./data_variables.js";
 
 //users
 
-webix.protoUI({
-    name: "editlist"
-}, webix.EditAbility, webix.ui.list);
-
 function sortNameAsc() {
-    $$(widgetsIds.usersListId).sort("#name#");
+  $$(widgetsIds.usersListId).sort("#name#");
 }
 
 function sortNameDesc() {
-    $$(widgetsIds.usersListId).sort("#name#", "desc");
+  $$(widgetsIds.usersListId).sort("#name#", "desc");
 }
 
 function deleteUser(ev, id) {
-    this.remove(id);
-    return false;
+  this.remove(id);
+  return false;
 }
 function addNewUser() {
-    const userList = $$(widgetsIds.usersListId);
-    const inputValue = $$(widgetsIds.usersInputId).getValue();
-    const userArr = inputValue.split(",")
-    console.log('input', userArr)
-    userList.add({  "name":userArr[0], "age":userArr[1], "country":userArr[2]})
+  const userList = $$(widgetsIds.usersListId);
+  const inputValue = $$(widgetsIds.usersInputId).getValue();
+  if (!inputValue) {
+    webix.message("Fill in the filed.");
+    return;
+  }
+  const userArr = inputValue.split(",");
+  userList.add({ name: userArr[0], age: userArr[1], country: userArr[2] });
 }
 
 const usersFilterAndSort = {
-    cols: [
-        {
-            view: "text",
-            id: widgetsIds.usersInputId,
-            on: {
-                onTimedKeyPress: function () {
-                    let value = this.getValue().toLowerCase();
-                    $$(widgetsIds.usersListId).filter(function (obj) {
-                        return obj.name.toLowerCase().indexOf(value) !== -1;
-                    });
-                },
-            },
+  cols: [
+    {
+      view: "text",
+      id: widgetsIds.usersInputId,
+      on: {
+        onTimedKeyPress: function () {
+          let value = this.getValue().toLowerCase();
+          $$(widgetsIds.usersListId).filter(function (obj) {
+            return obj.name.toLowerCase().indexOf(value) !== -1;
+          });
         },
-        {
-            view: "button",
-            autowidth: true,
-            value: "Sort asc",
-            click: sortNameAsc,
-        },
-        {
-            view: "button",
-            autowidth: true,
-            value: "Sort desc",
-            click: sortNameDesc,
-        },
-        {
-            view: "button",
-            autowidth: true,
-            value: "Add new",
-            click: addNewUser,
-        },
-    ],
+      },
+    },
+    {
+      view: "button",
+      autowidth: true,
+      value: "Sort asc",
+      click: sortNameAsc,
+    },
+    {
+      view: "button",
+      autowidth: true,
+      value: "Sort desc",
+      click: sortNameDesc,
+    },
+    {
+      view: "button",
+      autowidth: true,
+      value: "Add new",
+      click: addNewUser,
+    },
+  ],
 };
 
 const usersList = {
-    view: "editlist",
-    height: 200,
-    id: widgetsIds.usersListId,
-    editable: true,
-    editor: "text",
-    editValue: "name",
-    template:
-        "<div class='space user-list-style'>#name# from #country# <span class='deleteUser webix_icon wxi-close' ></span></div>",
-    select: true,
-    url: "./data/users.js",
-    scheme: {
-        $init: function (obj) {
-            if (obj.age < 26)
-                obj.$css = "yellow";
+  view: "editlist",
+  height: 200,
+  id: widgetsIds.usersListId,
+  editable: true,
+  editor: "text",
+  editValue: "name",
+  template:
+    "<div class='space user-list-style'>#name# from #country# <span class='deleteUser webix_icon wxi-close' ></span></div>",
+  select: true,
+  url: "./data/users.js",
+  scheme: {
+    $init: function (obj) {
+      if (obj.age < 26) obj.$css = "yellow";
+    },
+  },
+  onClick: {
+    deleteUser: deleteUser,
+  },
+  ready: function () {
+    $$(widgetsIds.chart).sync($$(widgetsIds.usersListId), function () {
+      this.group({
+        by: "country",
+        map: {
+          id: ["id", "count"],
         },
-
-    },
-    onClick: {
-        deleteUser: deleteUser,
-    },
-    ready: function () {
-        $$("my-chart").sync($$(widgetsIds.usersListId));
-
-    }
-    // ready: function () {
-    //     this.data.each(function (obj) {
-    //         const index = this.getIndexById(obj.id);
-    //         if (index < 5) {
-    //             $$(widgetsIds.usersListId).addCss(obj.id, "common");
-    //         }
-    //     });
-    // },
+      });
+    });
+  },
 };
 
 const userschart = {
-    view: "chart",
-    id: "my-chart",
-    type: "bar",
-    barWidth: 50,
-    // value: "#age#",
-    radius: 0,
+  view: "chart",
+  id: widgetsIds.chart,
+  type: "bar",
+  barWidth: 50,
+  radius: 0,
+  layout: "x",
+  value: "#id#",
+  xAxis: {
+    template: "#country#",
+  },
+  yAxis: {
+    start: 0,
+    step: 2,
+    end: 10,
+  },
+  legend: {
+    values: [{ text: "Country", color: "transparent" }],
+    valign: "bottom",
+    align: "center",
     layout: "x",
-    // label:"#country#",
-    value: "#id#",
-    xAxis: {
-        template: "#country#",
-    },
-    yAxis: {
-        start: 0,
-        step: 2,
-        end: 10
-    },
-    label: "#id#",
-    legend: {
-        values: [{ text: "Country", color: "transparent" }],
-        valign: "bottom",
-        align: "center",
-        layout: "x",
-    },
-    ready: function () {
-        $$("my-chart").group({
-            by: "country",
-            map: {
-                country: ["country", "count"]
-            }
-        });
-    }
+  },
 };
 
 const users = {
-    id: widgetsIds.users,
-    template: "Users View",
-    rows: [usersFilterAndSort, usersList, userschart],
+  id: widgetsIds.users,
+  template: "Users View",
+  rows: [usersFilterAndSort, usersList, userschart],
 };
 
-
+webix.protoUI(
+  {
+    name: "editlist",
+  },
+  webix.EditAbility,
+  webix.ui.list
+);
 
 export default users;
